c87db1142272ceb3f46254030deabd8fd35e6180
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-08-12 15:06:14 +0300

    Create Project

---
+++ Animals/Animal.swift
@@ -0,0 +1,24 @@
+//
+//  Animal.swift
+//  Animals
+//
+//  Created by Denis Bokov on 12.08.2022.
+//
+
+import Foundation
+
+struct Animal {
+    let name: String
+    let latin_name: String
+    let animal_type: String
+    let active_time: String
+    let length_min: Double
+    let length_max: Double
+    let weight_min: Double
+    let weight_max: Double
+    let lifespan: Double
+    let habitat: String
+    let diet: String
+    let geo_range: String
+    let image_link: String
+}
