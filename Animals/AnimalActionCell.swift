ea56935014b01597db4c731a604149117ae46e45
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-08-14 17:44:03 +0300

    Project revision

--- Animals/AnimalActionCell.swift
+++ Animals/AnimalActionCell.swift
@@ -9,4 +9,17 @@ import UIKit
 
 class AnimalActionCell: UICollectionViewCell {
     @IBOutlet var nameAnimalLebal: UILabel!
+    @IBOutlet var imageAnimalView: UIImageView!
+    
+    func configure(with animal: Animal) {
+        nameAnimalLebal.text = animal.name
+        
+        guard let url = URL(string: animal.image_link) else { return }
+        DispatchQueue.global().async { [weak self] in
+            guard let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                self?.imageAnimalView.image = UIImage(data: imageData)
+            }
+        }
+    }
 }
--- Animals/MainViewController.swift
+++ Animals/MainViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+
+
 class MainViewController: UICollectionViewController {
     
     let url = "https://zoo-animal-api.herokuapp.com/animals/rand/3"
@@ -24,20 +26,16 @@ class MainViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "animalButtonCell", for: indexPath) as? AnimalActionCell else { return UICollectionViewCell() }
         
-        cell.nameAnimalLebal.text = animals[indexPath.item].name
+        let animal = animals[indexPath.item]
+        cell.configure(with: animal)
+        
         return cell
     }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let infoAnimalVC = segue.destination as? InfoAboutAnimalViewController else { return }
-        infoAnimalVC.fetchAnimal()
-    }
-    
 }
 
 extension MainViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        CGSize(width: UIScreen.main.bounds.width - 48, height: 100)
+        CGSize(width: UIScreen.main.bounds.width - 48, height: 400)
     }
 }
 
@@ -53,7 +51,6 @@ extension MainViewController {
             
             do {
                 self?.animals = try JSONDecoder().decode([Animal].self, from: data)
-                print(self?.animals ?? "")
                 DispatchQueue.main.async {
                     self?.collectionView.reloadData()
                 }
